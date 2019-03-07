3aedcda330e654219bd1792a135b0bdb1ee1666b
sarah.morley <sarah.morley@enernoc.com>
2019-03-07 20:57:38 +0000

    Initial commit with Bubble Sort

---
+++ SortingAlgorithms/Algorithms.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingAlgorithms
+{
+    class Algorithms
+    {
+            static void Main(string[] args)
+            {
+                var sortList = new List<int>();
+                int[] numbers = { 2, 2, 5, 3, 1, 7, 6, 1, 8, 9 };
+                sortList.AddRange(numbers);
+
+                sortList = BubbleSort(sortList);
+                foreach (var number in sortList)
+                {
+                    Console.Write(number + " ");
+                }
+
+                Console.ReadLine();
+            }
+
+            public static List<int> BubbleSort(List<int> sortList2)
+            {
+                bool swap = true;
+                while (swap)
+                {
+                    swap = false;
+                    for (int z = 0; z < sortList2.Count - 1; z++)
+                    {
+                        if (sortList2[z] > sortList2[z + 1])
+                        {
+                            var zHolder = sortList2[z];
+                            sortList2[z] = sortList2[z + 1];
+                            sortList2[z + 1] = zHolder;
+                            swap = true;
+                        }
+                    }
+                }
+
+                return sortList2;
+            }
+        }
+    }
+
